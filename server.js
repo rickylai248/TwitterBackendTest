0dde3d416334fdfe2dc9806d88913e7f713a69e3
rickylai <f5x2b@ugrad.cs.ubc.ca>
2021-05-08 22:36:17 -0700

    Connecting Express.js to PostgresSQL

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: '{username}',
+        password: '{password}',
+        database: '{databasename}'
+    }
+});
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
+})
