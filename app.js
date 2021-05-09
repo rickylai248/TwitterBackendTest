26d472aa54a5a8c8a6b693dae41842cda0f741b8
rickylai <f5x2b@ugrad.cs.ubc.ca>
2021-05-08 22:43:46 -0700

    Installed Okta-nodejs packages

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var session = require("express-session");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
