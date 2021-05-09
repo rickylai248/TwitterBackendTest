39c7a51b7356d7a51f4799c1895d6e5b8e43b748
rickylai <f5x2b@ugrad.cs.ubc.ca>
2021-05-08 22:45:21 -0700

    Created session engine setup with secret string

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var session = require("express-session");
@@ -18,8 +18,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'LONG_RANDOM_STRING_HERE',
+  resave: true,
+  saveUninitialized: false
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
