5a833a0c7ec391f7a1cc37718fc37c1df93f0932
rickylai <f5x2b@ugrad.cs.ubc.ca>
2021-05-08 22:36:31 -0700

    Created controller "cocktail"

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const knex = require('knex')
 
+const cocktails = require('./controllers/cocktails')
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -17,6 +19,8 @@ const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+
+app.get('/cocktails', (req, res) => { cocktails.handleCocktails(req, res, db) })
 
 app.listen(3000, () => {
     console.log('app is running on port 3000')
